a0614d718acdafcc72cc753719e6e3b02c89c9e7
luis.ventura@tecsup.edu.pe <102069450+Angellvz@users.noreply.github.com>
2023-05-02 23:44:44 -0500

    v0.2

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
+// Importa las dependencias necesarias
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
+// Crea una instancia de la aplicación Express
 const app = express();
 
 app.use(express.static('public'));
-// Configurar el motor de plantillas
-app.set('view engine', 'pug');
-app.set('views', './views');
+// Configura el middleware body-parser para manejar las solicitudes POST
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// Configura el directorio de vistas para que Express cargue los archivos EJS
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.get('/'),(req)
+// Define la ruta de inicio
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
-// Iniciar el servidor en el puerto 5000
-app.listen(5000, () => {
-  console.log('Aplicación web dinámica ejecutándose en el puerto 5000');
+// Define la ruta de habilidades
+app.get('/habilidades', (req, res) => {
+  res.render('habilidades');
+});
+
+// Define la ruta de contacto
+app.get('/contacto', (req, res) => {
+  res.render('contacto');
+});
+
+// Inicia el servidor en el puerto 3000
+app.listen(3000, () => {
+  console.log('El servidor está en ejecución en el puerto 3000');
 });
